1ecfe18f9fc28c44f398eec15f04cfadb7657b36
Matias <Matibravoneron@gmail.com>
2025-07-23 09:37:32 -0300

    edit vite.configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/F1-Explorer-React-Vite-App/', // ← importante
   plugins: [
     react(),
     tailwindcss(),
